f5109292acfaedf7b8718582b8279a91884597a2
admin <pradeep.viramalla@gmail.com>
2017-08-12 15:40:46 +0530

    added vo

---
+++ src/main/java/com/honeywell/Fitnessapp/vo/UserDetails.java
@@ -0,0 +1,40 @@
+package com.honeywell.Fitnessapp.vo;
+
+public class UserDetails{
+	private String fullName;
+	private String emailAddress;
+	private String dob;
+	private String gender;
+	private String coach;
+	public String getFullName() {
+		return fullName;
+	}
+	public void setFullName(String fullName) {
+		this.fullName = fullName;
+	}
+	public String getEmailAddress() {
+		return emailAddress;
+	}
+	public void setEmailAddress(String emailAddress) {
+		this.emailAddress = emailAddress;
+	}
+	public String getDob() {
+		return dob;
+	}
+	public void setDob(String dob) {
+		this.dob = dob;
+	}
+	public String getGender() {
+		return gender;
+	}
+	public void setGender(String gender) {
+		this.gender = gender;
+	}
+	public String getCoach() {
+		return coach;
+	}
+	public void setCoach(String coach) {
+		this.coach = coach;
+	}
+	
+}

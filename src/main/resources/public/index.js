45fdf05c8388b9e4116bd186f44d818e17aedb06
admin <admin@IE1FLT3V1Q6C2.honeywell.com>
2017-08-12 14:40:15 +0530

    jsp changes

---
+++ src/main/resources/public/index.js
@@ -0,0 +1,23 @@
+var app = angular.module('myApp', []);
+app.controller('userdetails', function($scope,$http) {
+	$scope.user = {};
+	$scope.templeteUrl = "/register.html";
+	/*$http.get("/getuserfitnessdetails").then(function (response) {
+	      $scope.details = response.data;
+	 }); */  
+	
+	$scope.submitUserDetails = function(){
+		$scope.user.coach = $('#coach').val();
+		$scope.user.gender = $('#gender-male').val()
+		var config = {
+                headers : {
+                    'Content-Type': 'application/json'
+                }
+            }
+         $http.post('/saveuserdetails', $scope.user, config)
+         .then(function(data){$scope.templeteUrl = "/userdetails.html";
+         $scope.details = data.data.details;
+         $scope.coatchdetails = data.data.coatchdetails;
+	    });
+	}
+});
